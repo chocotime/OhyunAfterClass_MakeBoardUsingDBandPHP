45911262e5f609e2a90516bd852a4ca80ff808f7
unknown <Jungdeokho@gmail.com>
2014-12-05 17:17:18 +0900

    Update wrtie.php

--- wrtie.php
+++ wrtie.php
@@ -1,31 +1,60 @@
+<!DOCTYPE html>
 <html>
 <head>
 <title>게시판</title>
-<style>
-<!--
-td { font-size : 9pt, }
-A:link { font : 9pt; color : black, text-decoration : none;
-font-family : 굴림; font-size : 9pt; }
-A:visited { text-decoration : none; color : black; font-size : 9pt; }
-A:hover { text-decoration : underline; color : black;
-font-size : 9pt; }
--->
-</style>
 </head>
-<body topmargin=0 leftmargin=0 text=#464646>
+
+<body>
 <center>
-<BR>
-<!-- 입력된 값을 다음 페이지로 넘기기 위해 FORM을 만든다.-->
-<form action=insert.php method=post>
-<table width=580 border=0 cellpadding=2 cellspacing=1 bgcolor=#777777>
-<tr>
-<td height=20 align=center bgcolor=#999999>
-<font color=white> <B>글쓰기</B> </font>
-</td>
-</tr>
-<!-- 입력부분 -->
-<tr>
-<td bgcolor=white> &nbsp;
-<table>
-<tr>
-<td width=60 align=left> 이름 </td>
+<br>
+ <!-- 입력된 값을 다음 페이지로 넘기기 위해 FORM을 만든다. -->
+ <form action="insert.php" method="post">
+ <table width="800" border="1">
+     <tr>
+         <td align="center"><b>글 쓰 기</b></td>
+     </tr>
+     <!-- 입력 부분 -->
+     <tr>
+         <td>
+         <table>
+             <tr>
+                 <td align="left">이름</td>
+                 <td align="left"><input type="text" name="name" size="20" maxlength="10"></td>
+             </tr>
+             <tr>
+                 <td align="left">이메일</td>
+                 <td align="left"><input type="text" name="email" size="20" maxlength="25"></td>
+             </tr>
+             <tr>
+                 <td align="left">비밀번호</td>
+                 <td align="left">
+ 					<input type="password" name="pw" size="8" maxlength="8"> 
+                     (수정,삭제시 반드시 필요)
+                 </td>
+             </tr>
+             <tr>
+                 <td align="left">제 목</td>
+                 <td align="left"><input type="text" name="title" size="60" maxlength="35"></td>
+             </tr>
+             <tr>
+                 <td align="left">내용</td>
+                 <td align="left"><textarea name="content" cols="80" rows="15"></textarea></td>
+             </tr>
+             <tr>
+                 <td colspan="10" align="center">
+                     <input type="submit" value="글 저장하기">
+                     &nbsp;&nbsp;
+                     <input type="reset" value="다시 쓰기">
+                     &nbsp;&nbsp;
+                     <input type="button" value="되돌아가기" onClick="history.back(-1)">
+                 </td>
+             </tr>
+         </table>
+ </td>
+ </tr>
+ <!-- 입력 부분 끝 -->
+ </table>
+ </form>
+ </center>
+ </body>
+ </html>
